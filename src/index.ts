b18a49d65bba7cbdedf2abe215490dc8acd7654f
albaike <albaike@users.noreply.github.com>
2023-10-13 21:17:14 -0700

    Export `EndpointRuntime` to allow the use of this generic interface to endpoint bridges

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './types'
 export { isInternalEndpoint } from './internal/is-internal-endpoint'
 export { parseEndpoint } from './internal/endpoint'
+export { EndpointRuntime } from './internal/endpoint-runtime'
